7bb3a96841f5d768eb183148679e88db436ebdb8
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-12 16:56:51 +0100

    added colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,12 @@
 export const theme = {
     color: {
         liliac: "#FBFBFE",
+        slategray: "#6E7E91",
+        mineshaft: "#252525",
+        scienceblue: "#0366D6",
+        iron: "#D1D5DA",
+        white: "#FFFFFF",
+        mercury: "#E5E5E5",
     },
     breakpoint: {},
   };
