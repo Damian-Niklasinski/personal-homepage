b6445c839f106ea4b4e43c37b0b5b774c4710ed6
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-12 14:35:28 +0100

    index.css deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { GlobalStyle } from './GlobalStyle';
