a8de87d1fef8151f655a907904db949e8c8a8aef
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-12 19:46:22 +0100

    another styles to Portfolio component added

--- src/components/Portfolio/styled.js
+++ src/components/Portfolio/styled.js
@@ -4,15 +4,16 @@ export const StyledHeader = styled.header`
     max-width: 1216px;
     margin: auto;
     text-align: center;
+    margin-top: 72px;
 `;
 
 export const StyledImage = styled.img`
     width: 40px;
     height: 40px;
-    color: ${({theme}) => theme.color.scienceblue};
 `;
 
 export const StyledTitle = styled.h2`
     font-size: 30px;
     font-weight: 900;
+    margin-top: 12px;
 `;
