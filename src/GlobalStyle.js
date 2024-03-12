cda19a347e1cbd713d4f5c096d7b017deb796cda
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-12 14:18:26 +0100

    GlobalStyles added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'Inter', sans-serif;
+    word-break: break-word;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   </ThemeProvider>
