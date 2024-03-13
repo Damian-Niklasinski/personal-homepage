90d54d278680b70026068794126c16931426be02
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-13 19:39:38 +0100

    media query to wrapper added

--- src/components/Wrapper/styled.js
+++ src/components/Wrapper/styled.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
     padding: 119px 14px 109px;
+
+    @media (max-width: ${({theme}) => theme.breakpoint.mobile}) {
+        padding: 14px 14px 14px;
+    }
 `;
