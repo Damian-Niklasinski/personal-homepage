1ff9925acd88bd61eea44025bb0e699e82d2a01f
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-20 18:00:31 +0100

    Toggle Theme button now working

--- src/components/features/ThemeButton/index.js
+++ src/components/features/ThemeButton/index.js
@@ -1,10 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import { ThemeButtonContainer, StyledButtonText, StyledThemeButton } from "./styled";
+import { selectTheme, toggleTheme } from "./themeButtonSlice";
 
-const ChangeThemeButton = () => (
-    <ThemeButtonContainer>
-        <StyledButtonText>DARK MODE ON</StyledButtonText>
-        <StyledThemeButton />
-    </ThemeButtonContainer>
-);
+const ChangeThemeButton = () => {
+    const dispatch = useDispatch();
+    const darkTheme = useSelector(selectTheme);
+
+    return (
+        <ThemeButtonContainer>
+            <StyledButtonText>DARK MODE {darkTheme ? "ON" : "OFF"}</StyledButtonText>
+            <StyledThemeButton onClick={() => dispatch(toggleTheme())} />
+        </ThemeButtonContainer>
+    )
+};
 
 export default ChangeThemeButton;
