58a7ec394a9a58ea703977b3d05808778e81a272
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-20 18:23:08 +0100

    Button icon styled

--- src/components/features/ThemeButton/index.js
+++ src/components/features/ThemeButton/index.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
-import { ThemeButtonContainer, StyledButtonText, StyledThemeButton } from "./styled";
+import { ThemeButtonContainer, StyledButtonText, StyledThemeButton, StyledIconContainer, StyledSunIcon } from "./styled";
 import { selectTheme, toggleTheme } from "./themeButtonSlice";
+import SunIcon from "../../../images/sun-icon.svg"
 
 const ChangeThemeButton = () => {
     const dispatch = useDispatch();
@@ -9,7 +10,11 @@ const ChangeThemeButton = () => {
     return (
         <ThemeButtonContainer>
             <StyledButtonText>DARK MODE {darkTheme ? "ON" : "OFF"}</StyledButtonText>
-            <StyledThemeButton onClick={() => dispatch(toggleTheme())} />
+            <StyledThemeButton onClick={() => dispatch(toggleTheme())}>
+                <StyledIconContainer darkTheme={darkTheme}>
+                    <StyledSunIcon src={SunIcon} alt="SunIcon"/>
+                </StyledIconContainer>
+            </StyledThemeButton>
         </ThemeButtonContainer>
     )
 };
--- src/components/features/ThemeButton/styled.js
+++ src/components/features/ThemeButton/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ThemeButtonContainer = styled.div`
   display: flex;
@@ -6,22 +6,40 @@ export const ThemeButtonContainer = styled.div`
   align-items: center;
   margin-bottom: 65px;
 
-  @media (max-width: ${({theme}) => theme.breakpoint.mobile}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
         display: none;
     }
 `;
 
 export const StyledButtonText = styled.span`
-  color: ${({theme}) => theme.color.slategray};
+  color: ${({ theme }) => theme.color.slategray};
   font-size: 12px;
   font-weight: 700;
   margin-right: 12px;
 `;
 
 export const StyledThemeButton = styled.button`
-  width: 48px;
+  width: 55px;
   height: 26px;
   border: 1px solid #6E7E91;
   border-radius: 25px;
-  background-color: ${({theme}) => theme.color.mercury};
+  background-color: ${({ theme }) => theme.color.mercury};
+`;
+
+export const StyledIconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  background-color: ${({ theme }) => theme.color.slategray};
+  transition-duration: 250ms;
+
+  ${({ darkTheme }) => darkTheme && css`
+    transform: translateX(100%);
+  `}
+`;
+
+export const StyledSunIcon = styled.img`
+  align-self: center;
 `;
