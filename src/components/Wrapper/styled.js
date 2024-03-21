bf4c79f7d9476a8f6ff6c34b03056391fcd5d3fb
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-21 18:19:16 +0100

    added styling to Wrapper

--- src/components/Wrapper/styled.js
+++ src/components/Wrapper/styled.js
@@ -1,10 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 
 export const StyledWrapper = styled.div`
     padding: 119px 14px 109px;
+    font-family: 'Inter', sans-serif;
+    word-break: break-word;
+    background-color: ${({ theme }) => theme.color.liliac};
 
-    @media (max-width: ${({theme}) => theme.breakpoint.mobile}) {
+    ${({ $darkTheme }) => $darkTheme && css`
+    background-color: ${({ theme }) => theme.color.black};
+    `}
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
         padding: 14px 14px 14px;
     }
 `;
