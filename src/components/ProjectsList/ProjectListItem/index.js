a756e4f166fd0ea83064489862bc3a0fea5e9048
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-21 18:14:31 +0100

    added link to Demo and Code and styled it

--- src/components/ProjectsList/ProjectListItem/index.js
+++ src/components/ProjectsList/ProjectListItem/index.js
@@ -1,4 +1,4 @@
-import { StyledListDescription, StyledListHeader, StyledListItem } from "./styled";
+import { StyledListDescription, StyledListHeader, StyledListItem, StyledListLink } from "./styled";
 
 const ProjectListItem = ({title, description, demoLink, codeLink}) => {
     return (
@@ -6,8 +6,8 @@ const ProjectListItem = ({title, description, demoLink, codeLink}) => {
             <StyledListHeader>{title}</StyledListHeader>
             <StyledListDescription>{description}</StyledListDescription>
             <StyledListDescription>
-                {`Demo: ${demoLink}`}<br />
-                {`Code: ${codeLink}`}
+                {`Demo: `} <StyledListLink href={demoLink}>{demoLink}</StyledListLink><br />
+                {`Code: `} <StyledListLink href={codeLink}>{codeLink}</StyledListLink>
             </StyledListDescription>
         </StyledListItem>
     )
--- src/components/ProjectsList/ProjectListItem/styled.js
+++ src/components/ProjectsList/ProjectListItem/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledListItem = styled.li`
     border: 6px solid #D1D5DA4D;
     padding: 56px;
-    background-color: ${({theme}) => theme.color.white};
+    background-color: ${({ theme }) => theme.color.white};
     box-shadow: 
      0px 16px 58px 0px #090A3308,
      0px -2px 50px 0px #090A3305;
@@ -12,7 +12,7 @@ export const StyledListItem = styled.li`
         border: 6px solid #0366D633
     }
 
-    @media (max-width: ${({theme}) => theme.breakpoint.mobile}) {
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
         padding: 42px;
     }
 `;
@@ -30,4 +30,20 @@ export const StyledListDescription = styled.p`
     font-weight: 400;
     line-height: 25px;
     margin-top: 24px;
+`;
+
+export const StyledListLink = styled.a`
+    text-decoration: none;
+    transition-duration: 500ms;
+    color: ${({ theme }) => theme.color.iron};
+    border-bottom: 1px solid ${({ theme }) => theme.color.iron};
+
+    &:hover {
+        color: ${({ theme }) => theme.color.scienceblue};
+        border-bottom: 1px solid ${({ theme }) => theme.color.scienceblue};
+    }
+
+    &:visited {
+        color: ${({ theme }) => theme.color.scienceblue};
+    }
 `;
