50f2cb03f217be8d29b3c997aa51c0903f099e66
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-26 17:41:40 +0100

    Styles edited

--- src/components/Wrapper/Header/index.js
+++ src/components/Wrapper/Header/index.js
@@ -1,21 +1,37 @@
-import myImage from "../../../images/meImage2.jpg"
-import mailIcon from "../../../images/Message.png"
-import { FirstItem, HeaderItems, HireImage, HireMeButton, HireText, SecondItem, StyledHeader, StyledImage, ThirdItem } from "./styled";
+import myImage from "../../../images/ZDJECIE.jpg";
+import mailIcon from "../../../images/Message.png";
+import {
+  FirstItem,
+  HeaderItems,
+  HireImage,
+  HireMeButton,
+  HireText,
+  SecondItem,
+  StyledHeader,
+  StyledImage,
+  ThirdItem,
+} from "./styled";
 
 const Header = ({ HeaderContent, darkTheme }) => (
-    <StyledHeader>
-        <StyledImage src={myImage} alt="My picture" />
-        <HeaderItems>
-            {HeaderContent}
-            <FirstItem $darkTheme={darkTheme}>THIS IS</FirstItem>
-            <SecondItem $darkTheme={darkTheme}>Damian Niklasiński</SecondItem>
-            <ThirdItem $darkTheme={darkTheme}>👨🏻‍💻 I’m a passionate Frontend Developer in love with React,currently looking for new job opportunities.</ThirdItem>
-            <HireMeButton href="mailto: damianiklasinski@gmail.com" title="damianiklasinski@gmail.com">
-                <HireImage src={mailIcon} alt="MailIcon" />
-                <HireText>Hire Me</HireText>
-            </HireMeButton>
-        </HeaderItems>
-    </StyledHeader>
+  <StyledHeader>
+    <StyledImage src={myImage} alt="My picture" />
+    <HeaderItems>
+      {HeaderContent}
+      <FirstItem $darkTheme={darkTheme}>THIS IS</FirstItem>
+      <SecondItem $darkTheme={darkTheme}>Damian Niklasiński</SecondItem>
+      <ThirdItem $darkTheme={darkTheme}>
+        👨🏻‍💻 I’m a passionate Frontend Developer in love with React,currently
+        looking for new job opportunities.
+      </ThirdItem>
+      <HireMeButton
+        href="mailto: damianiklasinski@gmail.com"
+        title="damianiklasinski@gmail.com"
+      >
+        <HireImage src={mailIcon} alt="MailIcon" />
+        <HireText>Hire Me</HireText>
+      </HireMeButton>
+    </HeaderItems>
+  </StyledHeader>
 );
 
 export default Header;
--- src/components/Wrapper/Header/styled.js
+++ src/components/Wrapper/Header/styled.js
@@ -1,94 +1,97 @@
 import styled, { css } from "styled-components";
 
-
 export const StyledHeader = styled.header`
-    max-width: 1216px;
-    margin: auto;
-    display: grid;
-    grid-template-columns: auto 1fr;
-    grid-column-gap: 72px;
+  max-width: 1216px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-column-gap: 72px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-        grid-template-columns: none;
-        margin-top: 34px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-template-columns: none;
+    margin-top: 34px;
+  }
 `;
 
 export const StyledImage = styled.img`
-    width: 398px;
-    height: 398px;
-    border-radius: 50%;
+  max-width: 398px;
+  height: 398px;
+  border-radius: 5%;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-        width: 250px;
-        height: 250px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    max-width: 250px;
+  }
 `;
 
 export const HeaderItems = styled.div`
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-        margin-top: 16px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-top: 16px;
+  }
 `;
 
 export const FirstItem = styled.p`
-    font-size: 12px;
-    color: ${({ theme }) => theme.color.slategray};
-    font-weight: 700;
+  font-size: 12px;
+  color: ${({ theme }) => theme.color.slategray};
+  font-weight: 700;
 
-    ${({ $darkTheme }) => $darkTheme && css`
-        color: ${({ theme }) => theme.color.white};
+  ${({ $darkTheme }) =>
+    $darkTheme &&
+    css`
+      color: ${({ theme }) => theme.color.white};
     `}
 `;
 
 export const SecondItem = styled.h1`
-    font-size: 38px;
-    color: ${({ theme }) => theme.color.mineshaft};
-    font-weight: 900;
-    line-height: 46px;
-    margin-top: 12px;
+  font-size: 38px;
+  color: ${({ theme }) => theme.color.mineshaft};
+  font-weight: 900;
+  line-height: 46px;
+  margin-top: 12px;
 
-    ${({ $darkTheme }) => $darkTheme && css`
-        color: ${({ theme }) => theme.color.white};
+  ${({ $darkTheme }) =>
+    $darkTheme &&
+    css`
+      color: ${({ theme }) => theme.color.white};
     `}
 `;
 
 export const ThirdItem = styled.p`
-    font-size: 20px;
-    color: ${({ theme }) => theme.color.slategray};
-    font-weight: 400;
-    line-height: 28px;
-    letter-spacing: 5%;
-    margin-top: 35px;
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.slategray};
+  font-weight: 400;
+  line-height: 28px;
+  letter-spacing: 5%;
+  margin-top: 35px;
 
-    ${({ $darkTheme }) => $darkTheme && css`
-        color: ${({ theme }) => theme.color.white};
+  ${({ $darkTheme }) =>
+    $darkTheme &&
+    css`
+      color: ${({ theme }) => theme.color.white};
     `}
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-        margin-top: 0px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-top: 0px;
+  }
 `;
 
 export const HireMeButton = styled.a`
-    cursor: pointer;
-    width: 154px;
-    height: 49px;
-    margin-top: 32px;
-    background-color: ${({ theme }) => theme.color.scienceblue};
-    color: ${({ theme }) => theme.color.white};
-    border: 1px solid #D1D5DA4D;
-    border-radius: 4px;
-    display: flex;
-    gap: 16px;
-    justify-content: center;
-    align-items: center;
-    text-decoration: none;
+  cursor: pointer;
+  width: 154px;
+  height: 49px;
+  margin-top: 32px;
+  background-color: ${({ theme }) => theme.color.scienceblue};
+  color: ${({ theme }) => theme.color.white};
+  border: 1px solid #d1d5da4d;
+  border-radius: 4px;
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
 `;
 
-export const HireImage = styled.img`
-`;
+export const HireImage = styled.img``;
 
 export const HireText = styled.p`
-    font-weight: 600;
+  font-weight: 600;
 `;
