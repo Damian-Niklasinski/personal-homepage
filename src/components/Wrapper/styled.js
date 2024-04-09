7160cbeea6925db0d3efef0e857cf0b10ea85440
Damian Niklasinski <damianiklasinski@wp.pl>
2024-04-09 23:17:30 +0200

    wrapper padding edited

--- src/components/Wrapper/styled.js
+++ src/components/Wrapper/styled.js
@@ -1,18 +1,19 @@
 import styled, { css } from "styled-components";
 
-
 export const StyledWrapper = styled.div`
-    padding: 119px 14px 109px;
-    font-family: 'Inter', sans-serif;
-    word-break: break-word;
-    background-color: ${({ theme }) => theme.color.liliac};
-    transition-duration: 1s;
+  padding: 119px 14px 15px;
+  font-family: "Inter", sans-serif;
+  word-break: break-word;
+  background-color: ${({ theme }) => theme.color.liliac};
+  transition-duration: 1s;
 
-    ${({ $darkTheme }) => $darkTheme && css`
-    background-color: ${({ theme }) => theme.color.mineshaft};
+  ${({ $darkTheme }) =>
+    $darkTheme &&
+    css`
+      background-color: ${({ theme }) => theme.color.mineshaft};
     `}
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-        padding: 14px 14px 14px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    padding: 14px 14px 14px;
+  }
 `;
